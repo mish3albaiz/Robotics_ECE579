4dd72f9ba2804fa1d599b1aa3d31768f5e2078d6
Brian Henson <brian.henson1@yahoo.com>
2020-03-05 14:42:56 -0800

    update notes

---
+++ inmoov/runme_to_fix_windows_files.bash
@@ -0,0 +1,11 @@
+cd ~/catkin_ws/src/inmoov
+dos2unix */*.py
+chmod +x gui/body_poser_GUI.py
+chmod +x gui/body_poser_GUI_ros.py
+chmod +x gui/pose_organization_GUI.py
+chmod +x gui/animation_creator_GUI.py
+chmod +x scripts/body.py
+chmod +x scripts/workspace.py
+chmod +x testing/manual_calibration.py
+chmod +x head/camera_capture.py
+
