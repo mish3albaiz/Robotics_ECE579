ca936d66f657ff40439757a4168c551e8278a342
Brian Henson <brian.henson1@yahoo.com>
2020-03-05 16:07:41 -0800

    camera send/receive system

--- inmoov/head/camera_capture.py
+++ inmoov/head/camera_capture.py
@@ -19,13 +19,15 @@ def capture_image():
     # Create the in-memory stream
     stream = io.BytesIO()
     with picamera.PiCamera() as camera:
-        camera.start_preview()
-        time.sleep(2)
+        #camera.capture('/home/ubuntu/Desktop/foobar.jpg')
+        #camera.start_preview()
+        #time.sleep(2)
         camera.capture(stream, format='jpeg', resize=(360,240))
     # Construct a numpy array from the stream
     data = np.fromstring(stream.getvalue(), dtype=np.uint8)
     # "Decode" the image from the array, preserving colour
     image = cv2.imdecode(data, 1)
+    #cv2.imwrite("/home/ubuntu/Desktop/send.jpg", image)
     # OpenCV returns an array with data in BGR order. If you want RGB instead
     # use the following...
     # image = image[:, :, ::-1]
@@ -38,6 +40,8 @@ def publish_image(image_np):
     msg.header.stamp = rospy.Time.now()
     msg.format = "jpeg"
     msg.data = np.array(cv2.imencode('.jpg', image_np)[1]).tostring()
+    logme = "image capture: len=%d, time=%s" % (len(msg.data), rospy.get_time())
+    rospy.loginfo(logme)
     # Publish new image
     image_pub.publish(msg)
 
@@ -45,14 +49,11 @@ def main():
     global image_pub
     rospy.init_node('inmoov_head')
     image_pub = rospy.Publisher("/output/image_raw/compressed", CompressedImage)
-    rate = rospy.Rate(2) # twice per second
+    rate = rospy.Rate(2) # twice per second at most
     while not rospy.is_shutdown():
-        hello_str = "hello world %s" % rospy.get_time()
-        rospy.loginfo(hello_str)
         img = capture_image()
         publish_image(img)
         # taken_image = camera.capture() # not sure if this works
-        # pub.publish(hello_str)
         rate.sleep()
 
 if __name__ == '__main__':
---
+++ inmoov/head/camera_receive.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import roslib
+import rospy
+from sensor_msgs.msg import CompressedImage
+import cv2
+import numpy as np
+
+
+
+
+def callback_onimage(msg):
+    # takes a string-type message
+    logme = "image received: len=%d, time=%s" % (len(msg.data), rospy.get_time())
+    rospy.loginfo(logme)
+    print(type(msg.data))
+    data_np = np.fromstring(msg.data, dtype=np.uint8)
+    image = cv2.imdecode(data_np, 1)
+    cv2.imwrite("/home/ubuntu/Desktop/receive.jpg", image)
+    # note: cv2.imshow doesn't want to cooperate, sadly
+
+
+def main():
+    rospy.init_node('inmoov_head_receiver')
+    rospy.loginfo("bootup")
+    
+    rospy.Subscriber("/output/image_raw/compressed", CompressedImage, callback_onimage)
+    rospy.spin()
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except rospy.ROSInterruptException:
+        pass
