ea1e41f7ac4c3534f3941f36dace50a0f29cdbd4
Meshal <albaiz.meshal@gmail.com>
2020-03-07 11:28:55 -0800

    made division safe for python 2.7

--- inmoov/gui/body_poser_GUI.py
+++ inmoov/gui/body_poser_GUI.py
@@ -40,13 +40,13 @@ my_inmoov = Inmoov.Inmoov()
 class Application(tk.Frame):
     def __init__(self, master, on_change_callback):
         tk.Frame.__init__(self, master)
-        
+
         # this is the function to run when something changes and its time to send a message to ROS or inmoov
         # this is a function that takes a single string
         self.on_change_callback = on_change_callback
-        
+
         self.mode = 0	# mode 0=left, 1=center, 2=right
-        
+
         self.names_all = [["left_wrist",
                            "left_pinky",
                            "left_ring",
@@ -72,7 +72,7 @@ class Application(tk.Frame):
                             "right_shoulder_lift_out",
                             "right_shoulder_lift_front",
                             "right_arm_rotate"]]
-        
+
         # current_values is initiallized with all zeros
         self.current_values = []
         for p in self.names_all:
@@ -80,19 +80,19 @@ class Application(tk.Frame):
             for n in p:
                 s = my_inmoov.find_servo_by_name(n)
                 self.current_values[-1].append(0)
-        
+
         self.butt_init = tk.Button(master, text="INIT", command=self.inmoov_init)
         self.butt_init.grid(row=0, column=0, padx=button_padx, pady=button_pady)
-        
+
         self.butt_left = tk.Button(master, text="Left", command=lambda: self.changemode(0))
         self.butt_left.grid(row=0, column=1, padx=button_padx, pady=button_pady)
-        
+
         self.butt_center = tk.Button(master, text="Center", command=lambda: self.changemode(1))
         self.butt_center.grid(row=0, column=2, padx=button_padx, pady=button_pady)
-        
+
         self.butt_right = tk.Button(master, text="Right", command=lambda: self.changemode(2))
         self.butt_right.grid(row=0, column=3, padx=button_padx, pady=button_pady)
-        
+
         self.butt_off = tk.Button(master, text="OFF", command=self.inmoov_off)
         self.butt_off.grid(row=0, column=4, padx=button_padx, pady=button_pady)
 
@@ -101,10 +101,10 @@ class Application(tk.Frame):
 
         self.butt_save = tk.Button(master, text="SAVE", command=self.save_values)
         self.butt_save.grid(row=0, column=6, padx=button_padx, pady=button_pady, sticky=tk.W)
-        
+
         self.defaultcolor = self.butt_off.cget("background")
         print(self.defaultcolor)
-        
+
         # Scale.config(state=tk.DISABLED)
         # Scale.config(state=tk.NORMAL)
         # Label.config(text="asdf")
@@ -116,16 +116,16 @@ class Application(tk.Frame):
             self.labels.append(w)
             s = tk.Scale(master, from_=0, to=200, length=600, tickinterval=30, orient=tk.HORIZONTAL, command=self.get_changed_sliders)
             s.grid(row=i+1, column=1, columnspan=4)
-            
+
             self.sliders.append(s)
             # w.get() to return current slider val
             # w.set(x) to set initial value
             # resolution: default 1, set lower for floatingpoint
             # command: callback, gets value as only arg
-        
+
         self.changemode(0)
         pass
-    
+
     def changemode(self, newmode):
         if newmode==0:
             self.butt_left.config(bg="red")
@@ -139,7 +139,7 @@ class Application(tk.Frame):
             self.butt_right.config(bg="red")
         else:
             self.butt_right.config(bg=self.defaultcolor)
-        
+
         self.mode = newmode
         n = 0
         for n in range(len(self.names_all[self.mode])):
@@ -148,7 +148,7 @@ class Application(tk.Frame):
             self.labels[n].config(text=name)
             s = my_inmoov.find_servo_by_name(name)
             # min_angle, max_angle
-            tick = (s.max_angle - s.min_angle) / 4
+            tick = float(s.max_angle - s.min_angle) / 4.0
             # overwrite ranges & actual position of sliders
             self.sliders[n].config(state=tk.NORMAL, from_=s.min_angle, to=s.max_angle, tickinterval=tick, length=slider_fullsize)
             self.sliders[n].set(self.current_values[self.mode][n])
@@ -156,15 +156,15 @@ class Application(tk.Frame):
             # disable all other sliders
             self.labels[b].config(text="-----")
             self.sliders[b].config(state=tk.DISABLED, length=0)
-        
-    
+
+
     def inmoov_off(self):
         # make the actual inmoov turn off for ALL servos, not just currently active tab
         self.on_change_callback("off")
         # disable all sliders
         #for v in self.sliders:
         #	v.config(state=tk.DISABLED, length=0)
-    
+
     def inmoov_init(self):
         # make the actual inmoov run init for ALL servos, not just currently active tab
         self.on_change_callback("init")
@@ -181,7 +181,7 @@ class Application(tk.Frame):
                 if p == self.mode:
                     # set current values of all active sliders to their defaults
                     self.sliders[n].set(s.default_angle)
-    
+
     def get_changed_sliders(self, x):
         for i, n in enumerate(self.names_all[self.mode]):
             c = self.sliders[i].get()
@@ -201,7 +201,7 @@ class Application(tk.Frame):
                     self.current_values[self.mode][v] = c
                     self.sliders[v].set(c)
                     self.on_change_callback("left_torso" + "!" + str(c))
-        
+
 
     def save_values(self):
         # uses the name entered in the text box to append a new object onto the end of the existing "gestures.json" file
@@ -227,14 +227,13 @@ def actually_control_inmoov(message):
     # if running on the actual inmoov bot, we can use this callback to bypass ROS and directly hand off the messages
     print(message)
     my_inmoov.set_servo_ros(message)
-    
+
 def launch_gui(on_change_callback):
     root = tk.Tk()
     app = Application(root, on_change_callback)
     app.mainloop()
 
-    
+
 if __name__ == '__main__':
     # launch_gui(send_with_ros)
     launch_gui(actually_control_inmoov)
-
--- inmoov/scripts/frame_thread.py
+++ inmoov/scripts/frame_thread.py
@@ -19,7 +19,7 @@ import time
 
 INTERPOLATE_TIME = 0.1
 
-# uses the following members of "servo": 
+# uses the following members of "servo":
 # running_flag
 # idle_flag
 # _state_flag_lock
@@ -31,7 +31,7 @@ INTERPOLATE_TIME = 0.1
 def Frame_Thread_Func(servo, DEBUG_SERVO_ID):
     # looping forever
     while True:
-    
+
         # wait until servo."running" event is set by servo object
         servo.running_flag.wait()
 
@@ -47,7 +47,7 @@ def Frame_Thread_Func(servo, DEBUG_SERVO_ID):
                     frame = servo.frame_queue.pop(0)
             if frame is None:   # "else" but outside of the lock block
                 break
-                
+
             if servo.id == DEBUG_SERVO_ID:
                 print("%s: execute frame %s" % (servo.framethread_name, frame))
 
@@ -55,15 +55,15 @@ def Frame_Thread_Func(servo, DEBUG_SERVO_ID):
             # skip the safety checks in set_servo_angle or set_leg_position because safety is checked before interpolating
             # use the unprotected servo member function: also updates the position stored in the servo
             servo.do_set_servo_angle(frame[0])
-            
+
             # sleep for frame-delay
             time.sleep(frame[1])
             pass
-            
+
         # now frame queue is empty!
         if servo.id == DEBUG_SERVO_ID:
             print("%s: thread sleep" % servo.framethread_name)
-            
+
         with servo._state_flag_lock:
             # clear "running" event, does not trigger anything (note: clear before set)
             servo.running_flag.clear()
@@ -82,17 +82,16 @@ def Frame_Thread_Func(servo, DEBUG_SERVO_ID):
 # minimum duration is INTERPOLATE_TIME, rounds up if it is less
 def interpolate(dest, curr, dur):
     # find the delta(s)
-    delta = dest - curr
+    delta = float(dest - curr)
     # total time must not be less than INTERPOLATE_TIME
-    dur = max(dur, INTERPOLATE_TIME)
+    dur = float(max(dur, INTERPOLATE_TIME))
     # determine how many sections this time must be broken into: round up/down to nearest whole number
     num_frames = round(dur / INTERPOLATE_TIME)
     # from number of frames, calculate the time between each frame (should be very close to INTERPOLATE_TIME)
-    frame_time = dur / num_frames
+    frame_time = dur / float(num_frames)
     # build the list with list comprehension trickery
     # math is ((i+1)/num_frames * delta) + curr
     # the +1 is because the first frame should NOT be curr, and the final frame SHOULD be dest (curr+delta)
-    frame_list = [[((i+1)/num_frames * delta) + curr, frame_time] for i in range(num_frames)]
+    frame_list = [[(float(i+1)/float(num_frames) * delta) + curr, frame_time] for i in range(num_frames)]
     # done building the frame-list
     return frame_list
-
