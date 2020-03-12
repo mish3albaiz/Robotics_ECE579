2981116f898950a99777030769564b8628488c6f
Brian Henson <brian.henson1@yahoo.com>
2020-03-12 10:24:46 -0700

    added ROS version of animation GUI

---
+++ inmoov/gui/animation_creator_GUI_ros.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+
+import sys
+import animation_creator_GUI
+import rospy
+import std_msgs.msg as rosmsg
+
+
+
+def send_with_ros(message):
+    # i'm concerned that this might be sending too many messages over ROS, but oh well, we wont' know until we try
+    if not rospy.is_shutdown():
+        # print(message)
+        rospy.loginfo(message)
+        pub.publish(message)
+    
+    
+    
+if __name__ == '__main__':
+    try:
+        pub = rospy.Publisher('/inmoov/body/poser', rosmsg.String)
+        rospy.init_node('/inmoov/animation_gui', anonymous=True)
+        # launch the gui but give it the callback that sends over ROS instead of locally
+        animation_creator_GUI.launch_gui(send_with_ros)
+    except rospy.ROSInterruptException:
+        print("oops")
+        
+
+
