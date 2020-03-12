f8bc9975fee0fe9a7a0c2c6081858779b146cad6
Brian Henson <brian.henson1@yahoo.com>
2020-03-12 15:34:34 -0700

    added the notes that will be bundled with OS image

---
+++ runme_to_fix_windows_files.bash
@@ -0,0 +1,19 @@
+cd ~/catkin_ws/src/inmoov
+dos2unix */*.py
+chmod +x gui/body_poser_GUI.py
+chmod +x gui/body_poser_GUI_ros.py
+chmod +x gui/animation_creator_GUI.py
+chmod +x gui/animation_creator_GUI_ros.py
+chmod +x scripts/body.py
+chmod +x scripts/workspace.py
+chmod +x testing/manual_calibration.py
+chmod +x head/camera_capture.py
+chmod +x head/camera_receive.py
+
+cd ~/catkin_ws/src/file_transfer
+dos2unix */*.py
+chmod +x scripts/file_xfr.py
+chmod +x scripts/file_rcv.py
+
+
+
